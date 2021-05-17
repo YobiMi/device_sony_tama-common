cf997571d5ba25fc8fd53b85e7a8baa55c618c0a
Tim Murray <timmurray@google.com>
2021-05-17 15:56:48 -0700

    tama-common: preopt SystemUI
    SystemUI should always be speed-preopted on these devices.
    
    Test: flashed, SysUI is preopted
    Bug: 188465988
    Change-Id: I990ce9d7d8c08b8e80fe4ca0b73c9eb339f065f3

--- common.mk
+++ common.mk
@@ -345,6 +345,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libqti-perfd-client
 
+# Preopt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.sony_sdm845-libperfmgr \
