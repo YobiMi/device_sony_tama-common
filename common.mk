d62d1f6ff8c1b1d276bdc72b34d02f9446ec3b8f
ekkusa <dayesofficial@gmail.com>
2022-04-19 17:22:11 +0800

    tama-common: add overlay-blaze to common.mk

--- common.mk
+++ common.mk
@@ -21,7 +21,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    
+    $(LOCAL_PATH)/overlay-blaze
+        
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # A/B
