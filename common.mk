c2a61fc4ebcc47bbdadaaf5897ca5a5c9d4f06a2
Ubuntu <azureuser@Ubuntu.ugb2ssepe5tepepwo1vsh0dkqg.gx.internal.cloudapp.net>
2022-05-09 15:30:34 +0000

    tama-common: adapt

--- common.mk
+++ common.mk
@@ -21,7 +21,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -391,10 +391,6 @@ PRODUCT_BOOT_JARS += \
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
